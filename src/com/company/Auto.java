08889d1fef8abe7be808f28a586e6b12462649d9
Piotr2137 <piotrrachx@gmail.com>
2021-10-27 01:43:12 +0200

    2 pusch

--- src/com/company/Auto.java
+++ src/com/company/Auto.java
@@ -7,6 +7,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+
+
 enum KrajPochodzenia{
     NIEWIADOMY("-------"),
     NIEMCY("Niemcy"),
@@ -38,7 +40,7 @@ class AutoException extends Exception {
 
 } // koniec klasy PersonException
 
-public class Auto {
+public class Auto implements Comparable<Auto> {
 
 
     private String marka;
@@ -83,12 +85,72 @@ public class Auto {
     }
 
 
-
     @Override
     public String toString() {
         return marka + " " + model;
     }
 
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Auto auto = (Auto) o;
+
+        if (Double.compare(auto.pojemnosc_silnika, pojemnosc_silnika) != 0) return false;
+        if (marka != null ? !marka.equals(auto.marka) : auto.marka != null) return false;
+        if (model != null ? !model.equals(auto.model) : auto.model != null) return false;
+        return kraj == auto.kraj;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = marka != null ? marka.hashCode() : 0;
+        result = 31 * result + (model != null ? model.hashCode() : 0);
+        temp = Double.doubleToLongBits(pojemnosc_silnika);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (kraj != null ? kraj.hashCode() : 0);
+        return result;
+    }
+
+
+    @Override
+    public int compareTo(Auto that) {
+        if (this.getMarka() == null && that.getMarka() == null) {
+            // pass
+        } else if (this.getMarka() == null) {
+            return -1;
+        } else if (that.getMarka() == null) {
+            return 1;
+        } else {
+            int markaComparison = this.getMarka().compareTo(that.getMarka());
+            if (markaComparison != 0) {
+                return markaComparison < 0 ? -1 : 1;
+            }
+        }
+
+        if (this.getModel() == null && that.getModel() == null) {
+            // pass
+        } else if (this.getModel() == null) {
+            return -1;
+        } else if (that.getModel() == null) {
+            return 1;
+        } else {
+            int modelComparison = this.getModel().compareTo(that.getModel());
+            if (modelComparison != 0) {
+                return modelComparison < 0 ? -1 : 1;
+            }
+        }
+
+        if (this.getPojemnosc_silnika() != that.getPojemnosc_silnika()) {
+            return (this.getPojemnosc_silnika() < that.getPojemnosc_silnika() ? -1 : 1);
+        }
+
+        return 0;
+    }
 }
+
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,24 @@
 package com.company;
 
-import java.util.Arrays;
-import java.lang.Object;
-import java.util.Scanner;
+import java.util.*;
+import java.util.Map.Entry;
+
 
 public class Main {
 
+
+
     private static final String GREETING_MESSAGE =
             "Program Person - wersja konsolowa\n" +
-                    "Autor: spawacz sam\n" +
-                    "Data:  2137 r.\n";
+                    "Autor: Piotr Rachwalik\n" +
+                    "Data:  26.10.2021r.\n";
 
     private static final String MENU =
-            "    M E N U   G Ł Ó W N E  \n" +
-                    "1 - Stworz nowe auto \n" +
-                    "2 - Dodaj auto   \n" +
-                    "3 -    \n" +
-                    "4 -    \n" +
-                    "5 -    \n" +
+            "    M E N U   G Ł Ó W N E   \n" +
+                    "1 - Listy \n" +
+                    "2 - Sety   \n" +
+                    "3 - Map   \n" +
+                    "4 - Metody equals() i hashCode() - słowne wyjaśnienie  \n" +
                     "0 - Zakończ program   \n";
 
 
@@ -35,16 +36,22 @@ public class Main {
 
         while (true) {
             UI.clearConsole();
-            showCurrentAuto();
 
             try {
                 switch (UI.enterInt(MENU + "=>")) {
-                    case 1: //tworzenie nowego auta
-                        currentAuto = noweAuto();
+                    case 1:
+                        listy();
                         break;
                     case 2:
+                        sety();
                         break;
-                    case 0: //zakonczenie programu
+                    case 3:
+                        mapy();
+                        break;
+                    case 4:
+                        metody();
+                        break;
+                    case 0: //zakończenie programu
                         UI.printInfoMessage("\nProgram zakończył działanie!");
                         System.exit(0);
                 }
@@ -55,16 +62,224 @@ public class Main {
         }
     }
 
+    void listy(){
+
+        String MENU =
+                "    M E N U   L I S T  \n" +
+                        "1 - Stworz nowe auto \n" +
+                        "ArrayList \n" +
+                        "2 - Dodaj auto   \n" +
+                        "3 - Pokaż wszystkie auta   \n" +
+                        "4 - Usuń auto   \n" +
+                        "LinkedList \n" +
+                        "5 - Dodaj auto   \n" +
+                        "6 - Pokaż wszystkie auta   \n" +
+                        "7 - Usuń auto   \n" +
+                        "0 - Wróć   \n";
+
+        List<Auto> ArrayLista = new ArrayList<Auto>();
+        List<Auto> LinkLista = new LinkedList<Auto>();
+
+        boolean n=true;
+
+        while (n) {
+            UI.clearConsole();
+            showCurrentAuto();
+
+            try {
+                switch (UI.enterInt(MENU + "=>")) {
+                    case 1: //tworzenie nowego auta
+                        currentAuto = noweAuto();
+                        break;
+                    case 2: //dodawanie auta do listy (array)
+                        ArrayLista.add(currentAuto);
+                        break;
+                    case 3: //wypisanie listy (array)
+                        wypsanieListy(ArrayLista);
+                        break;
+                    case 4: //usuwanie auta (array)
+                        usuwanieListy(ArrayLista);
+                        break;
+                    case 5: //dodawanie auta do listy (linked)
+                        LinkLista.add(currentAuto);
+                        break;
+                    case 6: //wypisanie listy (linked)
+                        wypsanieListy(LinkLista);
+                        break;
+                    case 7: //usuwanie auta (linked)
+                        usuwanieListy(LinkLista);
+                        break;
+                    case 0: //powrót do menu głównego
+                        n=false;
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    void sety(){
+
+        String MENU =
+                "    M E N U   S E T Ó W  \n" +
+                        "1 - Stworz nowe auto \n" +
+                        "Hashset \n" +
+                        "2 - Dodaj auto   \n" +
+                        "3 - Pokaż wszystkie auta   \n" +
+                        "4 - Usuń auto   \n" +
+                        "Treeset \n" +
+                        "5 - Dodaj auto   \n" +
+                        "6 - Pokaż wszystkie auta   \n" +
+                        "7 - Usuń auto   \n" +
+                        "0 - Wróć   \n";
+
+        Set<Auto> hash = new HashSet<Auto>();
+        Set<Auto> tree = new TreeSet<Auto>();
+
+        boolean n=true;
+
+        while (n) {
+            UI.clearConsole();
+            showCurrentAuto();
+
+            try {
+                switch (UI.enterInt(MENU + "=>")) {
+                    case 1: //tworzenie nowego auta
+                        currentAuto = noweAuto();
+                        break;
+                    case 2: //dodawanie auta (hashset)
+                        hash.add(currentAuto);
+                        break;
+                    case 3: //wypisanie (hashset)
+                        wypisanieSety(hash);
+                        break;
+                    case 4: //usuwanie (hashset)
+                        usuwanieSety(hash);
+                        break;
+                    case 5: //dodawanie auta (treeset)
+                        tree.add(currentAuto);
+                        break;
+                    case 6: //wypisanie (treeset)
+                        wypisanieSety(tree);
+                        break;
+                    case 7: //usuwanie (treeset)
+                        usuwanieSety(tree);
+                        break;
+                    case 0: //powrót do menu głównego
+                        n=false;
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    void mapy(){
+
+        String MENU =
+                "    M E N U   M A P  \n" +
+                        "1 - Stworz nowe auto \n" +
+                        "HashMap \n" +
+                        "2 - Dodaj auto   \n" +
+                        "3 - Pokaż wszystkie auta   \n" +
+                        "4 - Usuń auto   \n" +
+                        "TreeMap \n" +
+                        "5 - Dodaj auto   \n" +
+                        "6 - Pokaż wszystkie auta   \n" +
+                        "7 - Usuń auto   \n" +
+                        "0 - Wróć   \n";
+
+        Map<Integer,Auto> hash = new HashMap<Integer,Auto>();
+        Map<Integer,Auto> tree = new TreeMap<Integer,Auto>();
+
+        boolean n=true;
+
+        while (n) {
+            UI.clearConsole();
+            showCurrentAuto();
+
+            try {
+                switch (UI.enterInt(MENU + "=>")) {
+                    case 1: //tworzenie nowego auta
+                        currentAuto = noweAuto();
+                        break;
+                    case 2: //dodawanie auta (hashmap)
+                        dodawanieMapy(hash,currentAuto);
+                        break;
+                    case 3: //wypisanie (hashmap)
+                        wypisanieMapy(hash);
+                        break;
+                    case 4: //usuwanie (hashmap)
+                        usuwanieMapy(hash);
+                        break;
+                    case 5: //dodawanie auta (treemap)
+                        dodawanieMapy(tree,currentAuto);
+                        break;
+                    case 6: //wypisanie (treemap)
+                        wypisanieMapy(tree);
+                        break;
+                    case 7: //usuwanie (treemap)
+                        usuwanieMapy(tree);
+                        break;
+                    case 0: //powrót do menu głównego
+                        n=false;
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    void metody(){
+
+        String MENU =
+                        "Przy niezdefiniowanych equals() oraz hashCode() \n" +
+                        "program rozróżnia różne obiekty o identycznych parametrach, \n" +
+                        "co nie pozwala na przykład usuwać konkretnych obiektów przez \n" +
+                        "podanie jego parametrów tak jak jest w programie w HashSet oraz TreeSet \n" +
+                        "po zaimplementowaniu equals() oraz hashCode() program uznaje je za ten sam obiekt. \n\n" +
+                        "0 - Wróć   \n";
+
+
+
+        boolean n=true;
+
+        while (n) {
+            UI.clearConsole();
+
+            try {
+                switch (UI.enterInt(MENU + "=>")) {
+                    case 0: //powrót do menu głównego
+                        n=false;
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+
+
+
         void showCurrentAuto(){
-        showAuto(currentAuto);
+            System.out.println("Aktualne auto:");
+            showAuto(currentAuto);
         }
 
         static void showAuto(Auto auto){
             StringBuilder sb = new StringBuilder();
 
             if (auto != null) {
-                sb.append("Aktualne auto: \n")
-                        .append("             Marka: ").append(auto.getMarka()).append("\n")
+                        sb.append("             Marka: ").append(auto.getMarka()).append("\n")
                         .append("             Model: ").append(auto.getModel()).append("\n")
                         .append("Pojemnosc silnika.: ").append(auto.getPojemnosc_silnika()).append("\n")
                         .append("  Kraj pochodzenia: ").append(auto.getKraj()).append("\n");
@@ -90,6 +305,8 @@ public class Main {
         KrajPochodzenia kraj= KrajPochodzenia.valueOf(scanner.next());
 
         Auto auto = new Auto(marka,model);
+        auto.setMarka(marka);
+        auto.setModel(model);
         auto.setPojemnosc_silnika(pojemnosc_silnika);
         auto.setKraj(kraj);
 
@@ -108,6 +325,62 @@ public class Main {
                 return null;
             }*////
             return auto;
+        }
+
+        static void dodawanieMapy( Map<Integer,Auto> mapa, Auto auto){
+
+            System.out.println("\nPodaj klucz do tego samochodu (liczba dziesiętna): ");
+            Scanner scanner=new Scanner(System.in);
+            int klucz= scanner.nextInt();
+            mapa.put(klucz,auto);
+        }
+
+        static void wypsanieListy( List<Auto> lista){
+
+            int i=1;
+
+            for( Auto auto : lista){
+                System.out.println(String.format("\n\nAuto nr %d",i));
+               showAuto(auto);
+               i++;
+            }
+        }
+
+        static void wypisanieSety( Set<Auto> set){
+
+            for(Auto auto : set){
+                showAuto(auto);
+            }
+        }
+
+        static void wypisanieMapy( Map<Integer,Auto> map){
+
+            Set<Entry<Integer,Auto>> entrySet = map.entrySet();
+            for(Entry<Integer, Auto> entry: entrySet) {
+                System.out.println(String.format("Klucz: %d",entry.getKey()));
+                showAuto(map.get(entry.getKey()));
+            }
+        }
+
+        static void usuwanieListy(List<Auto> lista){
+            System.out.println("\nPodaj numer auta z listy które chcesz usunąć: ");
+            Scanner scanner=new Scanner(System.in);
+            int nr= scanner.nextInt();
+            lista.remove(nr-1);
+        }
+
+        static void usuwanieSety(Set<Auto> set){
+            System.out.println("\nPodaj auto które chcesz usunąć: ");
+           Auto auto1=noweAuto();
+           set.remove(auto1);
+        }
+
+        static  void usuwanieMapy( Map<Integer,Auto> map){
+
+            System.out.println("\nPodaj klucz auta które chcesz usunąć: ");
+            Scanner scanner=new Scanner(System.in);
+            int klucz= scanner.nextInt();
+            map.remove(klucz);
         }
 
 
